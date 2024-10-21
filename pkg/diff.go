26a46abfc96e66a7380666358f5ecb154f5d075b
Jaroslaw Sitarczuk <jaroslaw.k.sitarczuk@gmail.com>
2024-10-21 16:05:15 +0200

    do not add delted files to package.xml

--- pkg/diff.go
+++ pkg/diff.go
@@ -9,7 +9,7 @@ import (
 const rootDir = "force-app/main/default"
 
 func GetChangedFilesByDirectory(sourceCommit, targetCommit, repoPath string) (map[string][]string, error) {
-	cmd := exec.Command("git", "diff", "--name-only", "--ignore-space-at-eol", "--ignore-space-change", "--ignore-all-space", sourceCommit, targetCommit)
+	cmd := exec.Command("git", "diff", "--name-status", "--ignore-space-at-eol", "--ignore-space-change", "--ignore-all-space", sourceCommit, targetCommit)
 	cmd.Dir = repoPath
 
 	output, err := cmd.Output()
@@ -20,13 +20,19 @@ func GetChangedFilesByDirectory(sourceCommit, targetCommit, repoPath string) (ma
 	changedFiles := strings.Split(strings.TrimSpace(string(output)), "\n")
 	result := make(map[string][]string)
 
-	for _, file := range changedFiles {
-		if !strings.HasPrefix(file, rootDir) {
+	for _, line := range changedFiles {
+		parts := strings.SplitN(line, "\t", 2)
+		if len(parts) != 2 {
+			continue
+		}
+
+		status, file := parts[0], parts[1]
+		if status == "D" || !strings.HasPrefix(file, rootDir) {
 			continue
 		}
 
 		relPath := strings.TrimPrefix(file, rootDir+"/")
-		parts := strings.SplitN(relPath, "/", 3)
+		parts = strings.SplitN(relPath, "/", 3)
 
 		if len(parts) < 2 {
 			continue
